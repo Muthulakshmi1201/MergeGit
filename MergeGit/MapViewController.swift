3ad221c7f9f38b6d0ccc53586d075b1a081f0c7e
Sharmila Kumarasamy <sharmila.kumarasamy@cognizant.com>
2018-01-25 11:31:38 +0530

    added Map View and some line of code

---
+++ MergeGit/MapViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MapViewController.swift
+//  MergeGit
+//
+//  Created by PACE on 1/25/18.
+//  Copyright © 2018 PACE. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Hello JJack")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
